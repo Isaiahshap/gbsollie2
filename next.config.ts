34650b5cf57e8565cea903ef89a1532d3d63d052
Yeshaya <andyshapiro613@gmail.com>
2025-11-26 13:15:46 -0500

    nextconfig

--- next.config.ts
+++ next.config.ts
@@ -23,9 +23,9 @@ const nextConfig: NextConfig = {
             key: 'Content-Security-Policy',
             value: [
               "default-src 'self'",
-              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com https://googletagmanager.com https://ssl.google-analytics.com",
-              "connect-src 'self' https://yeshayas.sg-host.com https://www.google-analytics.com https://analytics.google.com https://region1.google-analytics.com https://stats.g.doubleclick.net https://googletagmanager.com https://ssl.google-analytics.com https://*.google-analytics.com https://*.analytics.google.com https://*.googletagmanager.com",
-              "img-src 'self' data: https: https://www.google-analytics.com https://www.googletagmanager.com https://ssl.google-analytics.com https://*.google-analytics.com https://*.googletagmanager.com https://yeshayas.sg-host.com",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com https://googletagmanager.com https://ssl.google-analytics.com https://connect.facebook.net",
+              "connect-src 'self' https://yeshayas.sg-host.com https://www.google-analytics.com https://analytics.google.com https://region1.google-analytics.com https://stats.g.doubleclick.net https://googletagmanager.com https://ssl.google-analytics.com https://*.google-analytics.com https://*.analytics.google.com https://*.googletagmanager.com https://connect.facebook.net https://www.facebook.com",
+              "img-src 'self' data: https: https://www.google-analytics.com https://www.googletagmanager.com https://ssl.google-analytics.com https://*.google-analytics.com https://*.googletagmanager.com https://yeshayas.sg-host.com https://www.facebook.com",
               "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
               "font-src 'self' https://fonts.gstatic.com",
               "media-src 'self' https://yeshayas.sg-host.com",
